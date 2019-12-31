c57fb6d27a3bcc8b383e5d77ac84842caeaf6a83
Kelly Ann Marshall <kelly@kellyannmarshall.com>
2019-12-31 09:40:55 +0000

    Gooey footer

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ var scrollTriggers = document.querySelectorAll('.scroll');
 })
 
 document.querySelector('[data-hook="contact"]').addEventListener('click', (e) => {
-    document.querySelector('.animation').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
-    document.querySelector('input[name="name"]').focus();
+    document.querySelector('.footer').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
+    setTimeout(() => {
+        document.querySelector('input[name="name"]').focus();
+    }, 2000)
 })
 
 
@@ -34,87 +36,3 @@ document.getElementById("switch").addEventListener('click', (e) => {
     (page_container.classList.contains("offset") === true) ? page_container.classList.remove('offset') : page_container.classList.add('offset');
     (off_canvas_navigation.classList.contains("active") === true) ? off_canvas_navigation.classList.remove('active') : off_canvas_navigation.classList.add('active');
 })
-
-
-
-var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
-
-// Main
-initHeader();
-addListeners();
-
-function initHeader() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    target = { x: 0, y: height };
-
-    largeHeader = document.getElementById('animation');
-    largeHeader.style.height = height + 'px';
-
-    canvas = document.getElementById('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    ctx = canvas.getContext('2d');
-
-    // create particles
-    circles = [];
-    for (var x = 0; x < width * 0.5; x++) {
-        var c = new Circle();
-        circles.push(c);
-    }
-    animate();
-}
-
-// Event handling
-function addListeners() {
-    window.addEventListener('resize', resize);
-}
-
-function resize() {
-    width = window.innerWidth;
-    height = window.innerHeight / 2;
-    canvas.width = width;
-    canvas.height = height;
-}
-
-function animate() {
-    if (animateHeader) {
-        ctx.clearRect(0, 0, width, height);
-        for (var i in circles) {
-            circles[i].draw();
-        }
-    }
-    requestAnimationFrame(animate);
-}
-
-// Canvas manipulation
-function Circle() {
-    var _this = this;
-
-    // constructor
-    (function () {
-        _this.pos = {};
-        init();
-    })();
-
-    function init() {
-        _this.pos.x = Math.random() * width;
-        _this.pos.y = height + Math.random() * 100;
-        _this.alpha = 0.1 + Math.random() * 0.3;
-        _this.scale = 0.1 + Math.random() * 0.3;
-        _this.velocity = Math.random();
-    }
-
-    this.draw = function () {
-        if (_this.alpha <= 0) {
-            init();
-        }
-        _this.pos.y -= _this.velocity;
-        _this.alpha -= 0.00005;
-        ctx.beginPath();
-        ctx.arc(_this.pos.x, _this.pos.y, _this.scale * 10, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'rgba(255,255,255,' + _this.alpha + ')';
-        ctx.fill();
-    };
-}
-
